b8a303c9b8e50b83960ffc2ae53b47ba694a3f87
nabish00 <nabish00@gmail.com>
2018-09-21 17:49:24 +0500

    FromHome 21092018_05:50

--- routes/admin/AdminRoutes.js
+++ routes/admin/AdminRoutes.js
@@ -3,6 +3,8 @@ import express from 'express';
 var app = express();
 var router = express.Router();
 
-
+Router.post('/Register', (req, res) => {
+    
+})
 
 export default router;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import ('dotenv').config();
 import routes from './routes/routes';
 import AdminRoute from './routes/admin/AdminRoutes';
+
 
 var app = express();
 
 app.get("/",(req,res) => {
     res.send("Hello World")
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+
 
 app.use('/api',routes);
 app.use('/admin', AdminRoute);
