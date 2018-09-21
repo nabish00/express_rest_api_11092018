22def2b01e5012ea34d37e52fab0f5f9949a4208
nabish00 <nabish00@gmail.com>
2018-09-22 00:05:41 +0500

    From Home 22092018_00:05

---
+++ Controller/AdminController/Admin.js
@@ -0,0 +1,7 @@
+import mysql from 'mysql';
+
+export const CreateAdmin = (firstName, lastName, username, email, password, countryCode, 
+    regionCode, inputPhoneNumber, nationalNumber, e164Format, orginalFOrmat, internationalFormat,
+     isValidNumber, isValidNumberForRegion, address, city, state, country, street, addressTypeId) =>{
+
+}
